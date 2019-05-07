f43159daa3ae5d60e6ba56af62f97e7ad7e72df8
ChristopherRe <43577860+ChristopherRe@users.noreply.github.com>
2019-05-07 18:01:26 +0100

    Initial commit

---
+++ www/js/index.js
@@ -0,0 +1,10 @@
+//let document load before running this
+$(document).ready(function(){
+    
+    //used to set local storage of budget titles and ammounts.
+
+    //window.localStorage.setItem();
+
+    
+    
+});
